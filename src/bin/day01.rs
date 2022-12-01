3adafc86dd3898e29e28e7bfd35b23b0668554c4
Noah Wahl <noah.ares@yahoo.de>
2022-12-01 15:33:13 +0100

    Finish day01 on example data

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,4 +1,13 @@
 use anyhow::Result;
 
 fn main() -> Result<()> {
+    println!("{:?}", std::fs::read_to_string("./data/day01.example")?
+        .trim()
+        .split("\n\n")
+        .map(|block| block.split("\n").map(|item| item.parse::<i32>().unwrap()).sum())
+        .collect::<Vec<i32>>()
+        .iter()
+        .max()
+        .unwrap());
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
 use std::str::FromStr;
-use itertools::Itertools;
 
 pub fn read_one_per_line<T>(path: &str) -> Result<Vec<T>>
 where
@@ -32,25 +31,4 @@ T: FromStr,
        .split(sep)
        .filter_map(|c| c.parse::<T>().ok())
        .collect())
-}
-
-pub fn read_lines_sperated_by_empty_line<'a, T>(path: &'a str) -> Result<Vec<Vec<T>>>
-where
-T: FromStr, Vec<T>: FromIterator<&'a str>
-{
-    let mut result_split_by_empty_line = Vec::new();
-    let mut item = Vec::new();
-    for line in std::fs::read_to_string(path)?.lines() {
-        let it = line.trim();
-        if it.is_empty() {
-            result_split_by_empty_line.push(item);
-            item = Vec::new();
-        } else {
-            item.push(it);
-        }
-    }
-    if !item.is_empty() {
-        result_split_by_empty_line.push(item);
-    }
-    Ok(result_split_by_empty_line)
 }
