682387ea1681226dae1a7ae2e2daf4e06157b31b
Noah Wahl <noah.ares@yahoo.de>
2022-12-01 16:35:15 +0100

    Day1 task 2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,13 +1,23 @@
 use anyhow::Result;
 
 fn main() -> Result<()> {
-    println!("{:?}", std::fs::read_to_string("./data/day01.example")?
+    let mut sums = std::fs::read_to_string("./data/day01.input")?
         .trim()
         .split("\n\n")
         .map(|block| block.split("\n").map(|item| item.parse::<i32>().unwrap()).sum())
-        .collect::<Vec<i32>>()
+        .collect::<Vec<i32>>();
+        sums.sort_unstable_by(|a, b| b.cmp(a));
+
+    // part 1
+    println!("{:?}", sums
         .iter()
-        .max()
-        .unwrap());
+        .take(1)
+        .sum::<i32>());
+
+    // part 2
+    println!("{:?}", sums
+        .iter()
+        .take(3)
+        .sum::<i32>());
     Ok(())
 }
