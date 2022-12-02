57a9b7fcedd94290e96995caa6dc5864241ffd5d
Noah Wahl <noah.ares@yahoo.de>
2022-12-02 16:07:01 +0100

    Finish day02

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,21 +1,41 @@
 use anyhow::Result;
 
 fn main() -> Result<()> {
+
+    // part 1
     let result = std::fs::read_to_string("./data/day02.input")?
         .trim()
         .split("\n")
         .map(|block| block.split_once(" ").unwrap()).map(|x| match x {
-            ("A", "X") => 4,
-            ("A", "Y") => 1,
-            ("A", "Z") => 7,
-            ("B", "X") => 8,
-            ("B", "Y") => 5,
-            ("B", "Z") => 2,
-            ("C", "X") => 3,
-            ("C", "Y") => 9,
-            ("C", "Z") => 6,
+        ("A", "X") => 1 + 3,
+        ("A", "Y") => 2 + 6,
+        ("A", "Z") => 3 + 0,
+        ("B", "X") => 1 + 0,
+        ("B", "Y") => 2 + 3,
+        ("B", "Z") => 3 + 6,
+        ("C", "X") => 1 + 6,
+        ("C", "Y") => 2 + 0,
+        ("C", "Z") => 3 + 3,
+        (_, _) => 0,
+    }).sum::<i32>();
+    println!("{}", result);
+
+    // part 2
+    let result2 = std::fs::read_to_string("./data/day02.input")?
+        .trim()
+        .split("\n")
+        .map(|block| block.split_once(" ").unwrap()).map(|x| match x {
+            ("A", "X") => 3 + 0,
+            ("A", "Y") => 1 + 3,
+            ("A", "Z") => 2 + 6,
+            ("B", "X") => 1 + 0,
+            ("B", "Y") => 2 + 3,
+            ("B", "Z") => 3 + 6,
+            ("C", "X") => 2 + 0,
+            ("C", "Y") => 3 + 3,
+            ("C", "Z") => 1 + 6,
             (_, _) => 0,
         }).sum::<i32>();
-    println!("{}", result);
+    println!("{}", result2);
     Ok(())
 }
