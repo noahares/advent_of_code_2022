584689d5081999d13336db3e55d3604d6ed90dc7
Noah Wahl <noah.ares@yahoo.de>
2022-12-15 12:07:18 +0100

    Day15 setup

---
+++ src/bin/day15.rs
@@ -0,0 +1,15 @@
+use anyhow::Result;
+
+fn part01() -> Result<u32> {
+    Ok(0)
+}
+
+fn part02() -> Result<u32> {
+    Ok(0)
+}
+
+fn main() -> Result<()> {
+    println!("{:?}", part01().unwrap());
+    println!("{:?}", part02().unwrap());
+    Ok(())
+}
